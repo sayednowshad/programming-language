e77fe88881bb0091204d6ee12543b9ada3b19b54
Sayed Nowshad <166403040+sayednowshad@users.noreply.github.com>
2024-10-11 16:10:43 +0530

    Create Pascal_Triangle.py
    Using Python i did the pascal triangle which is asked in the Recent Interview also .

---
+++ Pascal_Triangle.py
@@ -0,0 +1,11 @@
+a = int(input("Enter The NUmber : "))
+
+s = [1]
+
+for i in range(a):
+  print(" " * (a-i) + " " .join(map(str,s))
+        k = [1]
+        for j in range(len(s) - 1 ):
+  k.appends(s[j] + s[j + 1 ])
+k.append(1)
+s = k
